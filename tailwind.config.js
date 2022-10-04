638aefd41b7ef683e44e3bf4354fa0e9f99c32b6
Anita <Efosa@Anita-Efosa.local>
2022-10-04 01:17:19 +0100

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+// module.exports = {
+//   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {
+//       screens: {
+//         sm: "480px",
+//         md: "768px",
+//         lg: "976px",
+//         xl: "1440px",
+//       },
+//       fontFamily: {
+//         sans: ["Graphik", "sans-serif"],
+//         serif: ["Merriweather", "serif"],
+//       },
+//     },
+//   },
+//   plugins: [require("@tailwindcss/line-clamp")],
+// };
